e5464e549da93998f572b486f8248105271f7fcd
Kieran O’Brien <hello@kieranobrien.dev>
2025-01-16 00:13:29 +0000

    Added first object/lights

--- src/main.js
+++ src/main.js
@@ -1,1 +1,47 @@
-import './style.css'
+import "./style.css";
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+); // field of view, aspect ratio, near, far
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(50);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.SphereGeometry(10, 32, 32);
+const material = new THREE.MeshStandardMaterial({ color: 0x006600 });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+const pointLight = new THREE.PointLight(0xffffff, 1000, 1000);
+pointLight.position.set(5, 20, 5);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight, pointLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+    requestAnimationFrame(animate);
+    sphere.rotation.y += 0.001;
+    pointLight.position.x = Math.sin(Date.now() * 0.001) * 20;
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
