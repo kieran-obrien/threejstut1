bf6fe8bee284a45c33ee0a3474f7b2af6c200868
Kieran O’Brien <hello@kieranobrien.dev>
2025-01-24 19:25:47 +0000

    Basic texture features

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+let readyToStart = false;
+
 // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -25,16 +27,11 @@ renderer.render(scene, camera);
 // Add orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// Load the texture and set it as the background
-const textureLoader = new THREE.TextureLoader();
-const earthTexture = textureLoader.load("/earth-texture.jpg");
-
 // Setting intial geometry and material for the sun and planets
 const geometrySun = new THREE.SphereGeometry(10, 32, 32);
-const geometryPlanet = new THREE.SphereGeometry(4, 15, 15);
 const materialSun = new THREE.MeshStandardMaterial({ color: 0xffaa0d });
 const material = new THREE.MeshStandardMaterial({ color: 0x006600 });
-const planetMaterial = new THREE.MeshPhongMaterial({map: earthTexture});
+const geometryPlanet = new THREE.SphereGeometry(4, 15, 15);
 const sun = new THREE.Mesh(geometrySun, materialSun);
 
 // Add the initial sun
@@ -50,6 +47,46 @@ const lightHelper = new THREE.PointLightHelper(pointLight);
 //const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper);
 
+// Load the planet textures
+const textureLoader = new THREE.TextureLoader();
+let textureCounter = 1;
+const planetTextures = [];
+
+let planetsArray;
+let planetMaterials = [];
+const loadTexture = (counter) => {
+    const texturePath = `./texture${counter}.jpg`;
+    console.log(`Attempting to load: ${texturePath}`);
+
+    textureLoader.load(
+        texturePath,
+        (texture) => {
+            // On successful load
+            console.log(`Successfully loaded: ${texturePath}`);
+            planetTextures.push(texture);
+            loadTexture(counter + 1); // Load the next texture
+            console.log(planetTextures);
+        },
+        undefined,
+        (err) => {
+            // On error (e.g., texture not found)
+            console.error(`Failed to load: ${texturePath}`, err);
+            console.log("Finished loading textures");
+            console.log(planetTextures);
+            [planetsArray, planetMaterials] = createPlanets(); // Create planets after loading all textures
+            console.log(planetsArray);
+        }
+    );
+};
+
+// Start loading textures
+loadTexture(textureCounter);
+const checkTexturesLoaded = setInterval(() => {
+    if (readyToStart) {
+        clearInterval(checkTexturesLoaded);
+    }
+}, 100);
+
 // Set planet class
 class Planet {
     constructor(size, distanceFromLast, orbitSpeed, mesh) {
@@ -61,6 +98,7 @@ class Planet {
         this.mesh = mesh;
         this.inOrbit = false;
         this.name = "";
+        this.textureCode = 0;
     }
     updatePlanetSize(size) {
         this.size = size;
@@ -70,46 +108,48 @@ class Planet {
         console.log(value);
         this.speed = speed;
         this.speedValue = value;
-        /*const transitionDuration = 1000; // Duration of the transition in milliseconds
-        const startTime = Date.now();
-        const startSpeed = this.previousSpeed;
-        const endSpeed = targetSpeed;
-
-        const animateTransition = () => {
-            const elapsedTime = Date.now() - startTime;
-            const progress = Math.min(elapsedTime / transitionDuration, 1);
-            this.speed = startSpeed + (endSpeed - startSpeed) * progress;
-
-            if (progress < 1) {
-                requestAnimationFrame(animateTransition);
-            }
-        };
-
-        animateTransition();
-        */
     }
 }
 
-let isPaused = false;
-// Pause and play functionality
-function pausePlay() {
-    console.log(isPaused)
-    isPaused = !isPaused;
-    console.log(isPaused);
-}
-const pauseButton = document.getElementById("pause-button");
-pauseButton.addEventListener("click", pausePlay);
-
 // Create and populate array of ten planet objects
+// Also create array of planet materials
+
 function getPlanet() {
-    return new Planet(1, 45, 0.0005, new THREE.Mesh(geometryPlanet, planetMaterial));
+    let planetMaterialInit = new THREE.MeshPhongMaterial({
+        map: planetTextures[0],
+    });
+    return new Planet(
+        1,
+        45,
+        0.0005,
+        new THREE.Mesh(geometryPlanet, planetMaterialInit)
+    );
 }
-const planetsArray = Array(10).fill().map(getPlanet);
-for (let i = 1; i < planetsArray.length + 1; i++) {
-    planetsArray[i - 1].name = `Planet ${i}`;
+
+function createPlanets() {
+    let materialCounter = 0;
+    console.log(planetTextures[materialCounter]);
+    planetMaterials = Array(planetTextures.length)
+        .fill()
+        .map(() => {
+            let material = new THREE.MeshPhongMaterial({
+                map: planetTextures[materialCounter],
+            });
+            materialCounter++;
+            return material;
+        });
+    const planetsArray = Array(10).fill().map(getPlanet);
+    for (let i = 1; i < planetsArray.length + 1; i++) {
+        planetsArray[i - 1].name = `Planet ${i}`;
+    }
+    readyToStart = true;
+    console.log("Planets created");
+    console.log(planetsArray);
+    return [planetsArray, planetMaterials];
 }
-console.log(planetsArray);
-console.log(planetsArray[0]);
+
+// console.log(planetsArray);
+// console.log(planetsArray[0]);
 
 // Add planet functionality
 let planetCount = 0;
@@ -128,6 +168,19 @@ function handlePlanets(planets) {
         planets[index].updatePlanetSpeed(speed / 5000, speed);
     };
     window.updatePlanetSpeed = updatePlanetSpeed;
+
+    // Update planet texture
+    const updatePlanetTexture = (index) => {
+        planets[index].textureCode++;
+        let nextTexture = planets[index].textureCode;
+        if (nextTexture >= planetMaterials.length) {
+            nextTexture = 0;
+            planets[index].textureCode = 0;
+        }
+        planets[index].mesh.material = planetMaterials[nextTexture];
+        planetMaterials[nextTexture].map.needsUpdate = true;
+    };
+    window.updatePlanetTexture = updatePlanetTexture;
 
     // Add planet control forms
     let planetCountCheck = planetCount;
@@ -166,7 +219,12 @@ function handlePlanets(planets) {
                 max="10"
                 value="${planetsArray[i].speedValue}"
             />
-            `;
+            <label for="planet-texture">Planet ${i + 1} Texture</label>
+            <input onclick="updatePlanetTexture(${i})"
+                type="button"
+                id="planet-texture-${i}"
+                name="planet-texture"
+            />`;
 
             // Set div to append control forms to
             const planetSizeDiv = document.getElementById(
@@ -222,12 +280,22 @@ function stars() {
 }
 stars();
 
+// Pause and play functionality
+let isPaused = false;
+function pausePlay() {
+    console.log(isPaused);
+    isPaused = !isPaused;
+    console.log(isPaused);
+}
+const pauseButton = document.getElementById("pause-button");
+pauseButton.addEventListener("click", pausePlay);
+
 // Function to animate the scene/loop
 function animate() {
-    arrow();
+    console.log(planetsArray);
     handlePlanets(planetsArray);
     let orbitRadius = 65;
-    if (!isPaused){
+    if (!isPaused) {
         for (const p of planetsArray) {
             // Smoothly transition to the new speed
             const orbitSpeed = Date.now() * p.speed;
@@ -246,4 +314,10 @@ function animate() {
     controls.update();
     renderer.render(scene, camera);
 }
-animate();
+
+const checkReadyToStart = setInterval(() => {
+    if (readyToStart) {
+        animate();
+        clearInterval(checkReadyToStart);
+    }
+}, 100);
